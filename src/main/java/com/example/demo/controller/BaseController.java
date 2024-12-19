f331aa1107048c6cc22c87fb2d60579dd5a8270f
Harry84911 <harrych.chang@dynasafe.com.tw>
2024-12-19 14:01:10 +0800

    v2

--- src/main/java/com/example/demo/controller/BaseController.java
+++ src/main/java/com/example/demo/controller/BaseController.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
 
-	@RequestMapping("/t1")
+	@GetMapping("/t1")
 	public String test() {
 		return "CICD Test";
 	}
