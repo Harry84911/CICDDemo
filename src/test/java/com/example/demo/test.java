a9bfac2a8c8e05c94edb1541291b9ddb82722b98
Harry84911 <harrych.chang@dynasafe.com.tw>
2025-01-13 15:00:24 +0800

    test webhook

---
+++ src/test/java/com/example/demo/test.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class test {
+	
+	@Test 
+	public void test() {
+		Double a = 0.0;
+		int b = 1;
+		System.out.println(a+b);
+	}
+
+}
